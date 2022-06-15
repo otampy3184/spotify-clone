087a7f1c76f94ad33e367b1cfe02258a5371aeca
django_freeman <o.tampodo.t.18@gmail.com>
2022-06-15 23:04:16 +0900

    refresh

--- server/server.js
+++ server/server.js
@@ -11,6 +11,31 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+// refresh
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: process.env.REDIRECT_URI,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken,
+    })
+  
+    spotifyApi
+      .refreshAccessToken()
+      .then(data => {
+        res.json({
+          accessToken: data.body.accessToken,
+          expiresIn: data.body.expiresIn,
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.sendStatus(400)
+      })
+  })
+  
+
 // login
 app.post("/login", (req, res) => {
     const code = req.body.code
