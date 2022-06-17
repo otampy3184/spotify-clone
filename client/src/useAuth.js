ce35876923f04d91babcf26774f60ce24b942ba9
django_freeman <o.tampodo.t.18@gmail.com>
2022-06-17 23:36:14 +0900

    副作用追加

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function useAuth(code) {
     const [accessToken, setAccessToken] = useState()
     const [refreshToken, setRefreshToken] = useState()
-    const [exporesIn, setExpiresIn] = useState() 
+    const [exporesIn, setExpiresIn] = useState()
 
     useEffect(() => {
         axios
@@ -21,4 +21,25 @@ export default function useAuth(code) {
                 window.location = "/"
             })
     }, [code])
+
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+            axios
+                .post("http://localhost:3001/refresh", {
+                    refreshToken,
+                })
+                .then(res => {
+                    setAccessToken(res.data.accessToken)
+                    setExpiresIn(res.data.expiresIn)
+                })
+                .catch(() => {
+                    window.location = "/"
+                })
+        }, (expiresIn - 60) * 1000)
+
+        return () => clearInterval(interval)
+    }, [refreshToken, expiresIn])
+
+    return accessToken
 }
